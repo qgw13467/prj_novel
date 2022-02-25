fa8f5b77ed15b0f5cdebc29cd0a5437d6926856c
qgw13467 <51719533+qgw13467@users.noreply.github.com>
2022-02-25 15:45:55 +0900

    swagger3

--- src/main/java/io/team/PrjNovelApplication.java
+++ src/main/java/io/team/PrjNovelApplication.java
@@ -4,10 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan(basePackages = "io.team.mapper")
+@EnableWebMvc
 public class PrjNovelApplication {
 
 
